03f3432b3e428d9cc528bde3ba093857c58efaf9
Igor Lisboa <igor.lisboa@icraft.com.br>
2020-01-16 23:20:16 -0300

    preparando mapview p abrir na localizacao do usuario

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,23 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
+    const [currentRegion, setCurrentRegion] = useState(null);
+
     useEffect(() => {
         async function loadInitialPosition() {
             const { granted } = await requestPermissionsAsync();
 
             if (granted) {
-                const location = await getCurrentPositionAsync({
+                const { coords } = await getCurrentPositionAsync({
                     enableHighAccuracy: true,
                 });
+
+                const { latitude, longitude } = coords;
+                setCurrentRegion(
+                    {
+                        latitude,
+                        longitude,
+                        latitudeDelta: 0.04,
+                        longitudeDelta: 0.04,
+                    }
+                );
             }
         }
 
         loadInitialPosition();
     }, []);
-    return <MapView style={styles.map} />
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map} />
+    );
 }
 
 const styles = StyleSheet.create({
